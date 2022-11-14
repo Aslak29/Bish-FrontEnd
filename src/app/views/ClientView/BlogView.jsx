607752d9c4ca812041c087ed91ce07377d3cfb25
corentin27 <tailleur.corentin27@gmail.com>
2022-11-14 15:26:00 +0100

    Test api avec BlogController

--- src/app/views/ClientView/BlogView.jsx
+++ src/app/views/ClientView/BlogView.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import apiBackend from "../../api/backend/api.Backend";
+import {URL_BACK_BLOG} from "../../constants/urls/urlBackEnd";
 
 const BlogView = () => {
+
+  const [blog,setBlog] = useState([]);
+
+  useEffect(() => {
+    apiBackend.get(URL_BACK_BLOG).then((response => {
+      setBlog(response.data)
+      console.log(response.data)
+    }))
+  },[])
+
   return (
-    <div>BlogView</div>
+    <div>
+      BlogView
+      {blog.map((blogs) => {
+        return (
+            <h1>{blogs.title}</h1>
+        )
+      } )}
+    </div>
   )
 }
 
