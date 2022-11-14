d2fba752aa7a627ac4b188f3f2ddff42df599b0f
Aslak29 <alexandre.cholewa@outlook.fr>
2022-11-14 13:49:08 +0100

    footer design

--- src/app/components/layouts/Footer.jsx
+++ src/app/components/layouts/Footer.jsx
@@ -9,28 +9,38 @@ import {
     URL_CGU,
     URL_HELP,
   } from "../../constants/urls/urlFrontEnd"
+  import logo from "../../assets/images/logo-bish.svg";
+  import stripe from "../../assets/images/stripe.svg";
 
 function Footer() {
   return (
     <footer className="bish-bg-blue bish-text-white">
-        <div className='flex justify-between items-center pt-6 pb-6 pl-3 pr-3'>
-            <div>Logo</div>
+        <div className='flex justify-between items-center pt-4 pb-4 pl-4 pr-2 sm:pt-6 sm:pb-6 sm:pl-10 sm:pr-10'>
+            <div className='order-last sm:order-first'> <img
+              className="h-12 w-auto sm:h-20"
+              src={logo}
+              alt="Logo Bish"
+            /></div>
             <div>
-                <ul>
+                {/* <ul>
                     <Link to={URL_PRESENTATION}><li>Qui sommes-nous ?</li></Link>
                     <Link to={URL_BLOG}><li>Blog</li></Link>
                     <Link to={URL_CONTACT}><li>Contact</li></Link>
-                </ul>
+                    <Link to={URL_LEGAL_NOTICE}><li>Mentions légales</li></Link>
+                </ul> */}
             </div>
             <div>
-                <ul>
-                <Link to={URL_LEGAL_NOTICE}><li>Mentions légales</li></Link>
+                {/* <ul>
                 <Link to={URL_CGU}><li>CGU</li></Link>
                 <Link to={URL_FAQ}><li>FAQ</li></Link>
-                <Link to={URL_HELP}> <li>Aide</li></Link>
-                </ul>
+                <Link to={URL_HELP}><li>Aide</li></Link>
+                </ul> */}
             </div>
-            <div>Stripe</div>
+            <div><img
+              className="hidden w-auto sm:block sm:h-10"
+              src={stripe}
+              alt="stripe"
+            /></div>
         </div>
     </footer>
   )
