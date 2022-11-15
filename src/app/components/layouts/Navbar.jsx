9007ea00444a15f0d3529fa410f87564c9a7447b
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2022-11-15 13:49:47 +0100

    Avancement Navbar-PC ok

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 // import { useSelector } from "react-redux";
 // import { selectIsLogged } from "./../../redux-store/authenticationSlice";
@@ -13,33 +13,42 @@ import {
 import logo from "../../assets/images/logo-bish.svg";
 import Account from "../../assets/images/Account.svg";
 import Panier from "../../assets/images/Panier.svg";
+import search from "../../assets/images/search.svg";
 
 const Navbar = () => {
   // const isLoggued = useSelector(selectIsLogged);
-  // const [showLinks, setShowLinks] = useState(false);
-  // const handleShowLinks = () =>{
-  //   setShowLinks(!showLinks);
-  // }
+
+  // ---------------------States et Fonction pour le Menu Burger--------------------
+  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const updateMenu = () =>{
+    if(!isMenuClicked){
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+      console.log(burgerClass);
+    }else{
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+  }
+
   return (
-    <div className="absolute mx-auto w-full bish-bg-blue px-4 shadow-sm sm:px-6 flex flex-row justify-between content-center ">
+    <div className="absolute w-full bish-bg-blue px-1 sm:px-4 flex flex-row justify-between content-center ">
       
       {/*--------------------------------- Menu Burger---------------------------------  */}
-      <div className="block w-10 h-10 m-auto hover:cursor-pointer sm:hidden">
-        {/* <div className="burger-bar bish-bg-white w-10 h-2 m-auto"></div>
-        <div className="burger-bar bish-bg-white w-10 h-2 m-auto"></div>
-        <div className="burger-bar bish-bg-white w-10 h-2 m-auto"></div> */}
-        <button>
-          <span className="bish-bg-white w-10 h-4 flex flex-col gap-x-1 border
-          after:content-['after'] after:translate-y-3 after:bish-bg-white after:border
-          before:content-['before'] before:-translate-y-3 before:bish-bg-white before:border">Milieu</span>
-        </button>
+      <div className={`flex flex-col gap-y-2 w-10 h-10 m-auto hover:cursor-pointer sm:hidden`} onClick={updateMenu}>
+        <div className={`${burgerClass} w-12 h-1.5 bish-bg-white m-auto`}></div>
+        <div className={`${burgerClass} w-12 h-1.5 bish-bg-white m-auto`}></div>
+        <div className={`${burgerClass} w-12 h-1.5 bish-bg-white m-auto`}></div>
       </div>
+      <div className={menuClass}></div>
 
       {/*--------------------------------- LOGO ---------------------------------  */}
       <div>
         <Link to={URL_HOME}>
           <img
-            className="h-20 w-auto cursor-pointer"
+            className="h-20 w-20 cursor-pointer"
             src={logo}
             alt="Logo Bish"
           />
@@ -48,31 +57,29 @@ const Navbar = () => {
         
       {/*---------------------------------  Liens navbar --------------------------------- */}
 
-      <div className="hidden flex bish-text-white h-8 my-auto sm:block">
-        <ul className="flex flex-direction: row  h-8">
+      <div className={`hidden bish-text-white h-8 my-auto sm:block mx-20`}>
+        <ul className="flex flex-row h-8">
           <li className="w-auto border-l px-2 flex justify-center">
-            <Link to={URL_PRODUCTS}>
+            <Link to={URL_PRODUCTS} className="mx-2 font-bold">
               Nos Produits
             </Link>            
           </li>
           <li className="w-auto border-l px-2 flex justify-center">
-            <Link to={URL_PRESENTATION}>
+            <Link to={URL_PRESENTATION} className="mx-2 font-bold">
               Qui sommes-nous?
             </Link>
           </li>
           <li className="w-auto border-l border-r px-2 flex justify-center">
-            <Link to={URL_BLOG}>
+            <Link to={URL_BLOG} className="mx-2 font-bold">
               Blog
             </Link>
           </li>
-
-
         </ul>
       </div>
 
       {/*---------------------------------  Searchbar ---------------------------------  */}
 
-      <div className="h-8 my-auto w-80 ">
+      <div className="h-8 my-auto mx-0 sm:w-72">
         <form action="#" className="search-form hidden sm:block">
           <input 
             className="h-8 rounded-full w-72 border-transparent" 
@@ -80,26 +87,30 @@ const Navbar = () => {
             name="searchbar" 
             id="searchbar" 
             placeholder="Rechercher..."/>
+            <input type="submit" value="" className="hidden"/>
         </form>
-        <button>
-          <img src="" alt="" />
-        </button>
-      </div>
+          <button className="sm:hidden">
+            <img
+                className="h-8 w-auto cursor-pointer"
+                src={search}
+                alt="Panier"/> 
+          </button>
+     </div>
 
       {/*--------------------------------- Espace client et panier ---------------------------------  */}
 
-      <div className="flex flex-col space-y-4 md:flex-row md:items-center md:space-y-0 md:space-x-4">
-          <>
+      <div className="flex justify-end my-auto mx-0 gap-x-8">
+          <> 
             <Link to={URL_SHOPPING_CART}>
               <img
-                className="h-8 w-auto cursor-pointer sm:h-10"
+                className="h-8 w-auto cursor-pointer sm:h-8"
                 src={Panier}
                 alt="Panier"
               />                
             </Link>
             <Link to={URL_INFOS}>
               <img
-                className="h-8 w-auto cursor-pointer sm:h-10"
+                className="h-8 w-auto cursor-pointer sm:h-8"
                 src={Account}
                 alt="Espace Client"
               />                    
