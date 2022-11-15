16b66c104f6eee6972da1f4059fe6b0dc5755d9a
Aslak29 <alexandre.cholewa@outlook.fr>
2022-11-15 15:12:45 +0100

    Gestion erreur formulaire pour nom prenom mdp mail

--- src/app/components/account/Register.jsx
+++ src/app/components/account/Register.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Field, Form, Formik } from "formik";
 import { Link } from "react-router-dom";
-import loginSVG from "../../assets/images/register-view-login.svg"
-import {
-  URL_LOGIN,
-} from "../../constants/urls/urlFrontEnd"
+import { useState } from "react";
+import { handleChange } from "react";
+import loginSVG from "../../assets/images/register-view-login.svg";
+import { URL_LOGIN } from "../../constants/urls/urlFrontEnd";
 
-const Register = () => {
+const Register = (props) => {
+  const [long, longEnough] = useState(false);
+  const [number, hasNumber] = useState(false);
+  const [name, hasName] = useState(false);
+  const [surname, hasSurname] = useState(false);
+  
   return (
     <div className="flex items-center justify-center sm:mt-20 mt-20 my-10 ">
       <div className="flex items-center justify-center flex-col border-2 border-black rounded-3xl w-3/4">
@@ -17,67 +22,130 @@ const Register = () => {
               email: "",
               password: "",
               confirmation: "",
-              nom: "",
-              prenom: "",
+              name: "",
+              surname: "",
             }}
-            // onSubmit={handleRegister}
+            validate={(values) => {
+              values.password.length < 8 ? longEnough(false) : longEnough(true);
+              !/\d/.test(values.password) ? hasNumber(false) : hasNumber(true);
+              /^([a-zA-Z ]+)$/.test(values.name) ? hasName(false) : hasName(true);
+              /^([a-zA-Z ]+)$/.test(values.surname) ? hasSurname(false) : hasSurname(true);
+            }}
+            onSubmit={(values,{resetForm, setErrors, setSubmitting, handleRegister}) => {
+              let errors = {};
+              if (name)
+              errors.name =
+              "Veulliez renseigner votre Prénom"
+              console.log(errors.name)
+              if (surname)
+              errors.surname =
+              "Veulliez renseigner votre Nom"
+              console.log(errors.surname)
+
+              if (!long || !number)
+                errors.password =
+                  "Votre mot de passe doit contenir 8 caractères et un chiffre";
+                console.log(errors.password);
+                if (
+                  Object.entries(errors).length === 0 &&
+                  errors.constructor === Object
+                ) {
+                  alert(
+                    `Vous avez bien crée votre compte avec ${values.email}`
+                  );
+                  resetForm();
+                } else {
+                  setErrors(errors);
+                }
+                setSubmitting(false);
+
+            }}
           >
-            <Form className="flex justify-center w-full">
-              <div className="flex justify-center flex-col sm:flex-row rounded-md shadow-sm pt-10 pb-10 w-3/4">
-                <div className="w-full sm:pr-5 space-y-6 sm:w-1/2">
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Addresse e-mail"
-                    className="input"
-                  />
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Mot de passe"
-                    className="input"
-                  />
-                  <Field
+            {({ errors, values, handleChange, handleSubmit, isSubmitting }) => (
+              <Form
+                className="flex justify-center w-full"
+                onSubmit={handleSubmit}
+              >
+                <div className="flex justify-center flex-col sm:flex-row rounded-md shadow-sm pt-10 pb-10 w-3/4">
+                  <div className="w-full sm:pr-5 space-y-6 sm:w-1/2">
+                    <Field
+                      type="email"
+                      name="email"
+                      placeholder="Addresse e-mail"
+                      className="input"
+                      onChange={handleChange}
+                      required
+                      value={values.email}
+                    />
+                    <Field
+                      type="password"
+                      name="password"
+                      placeholder="Mot de passe"
+                      className="input"
+                      onChange={handleChange}
+                      required
+                      value={values.password}
+                    />
+                          {errors.password && (
+                      <label className="error" htmlFor="password">
+                        Sorry! {errors.password}
+                      </label>
+                    )}
+                    {/* <Field
                     type="confirmation"
                     name="confirmation"
                     placeholder="Confirmer le mot de passe"
-                    className="input"
-                  />
+                    className="input"       onChange={handleChange}
+                    required
+                    value={values.confirmation}
+                  /> */}
+                  </div>
+                  <div className="w-full pt-6 sm:pt-0 space-y-6 sm:w-1/2">
+                    <Field
+                      type="surname"
+                      name="surname"
+                      placeholder="Nom"
+                      className="input"
+                      onChange={handleChange}
+                      required
+                      value={values.surname}
+                    />
+                    <Field
+                      type="name"
+                      name="name"
+                      placeholder="Prénom"
+                      className="input"
+                      onChange={handleChange}
+                      required
+                      value={values.name}
+                    />
+                    <button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="bish-bg-blue py-2 rounded-3xl w-full bish-text-white shadow-lg "
+                    >
+                      S'inscrire
+                    </button>
+                  </div>
                 </div>
-                <div className="w-full pt-6 sm:pt-0 space-y-6 sm:w-1/2">
-                  <Field
-                    type="nom"
-                    name="nom"
-                    placeholder="Nom"
-                    className="input"
-                  />
-                  <Field
-                    type="prenom"
-                    name="prenom"
-                    placeholder="Prénom"
-                    className="input"
-                  />
-
-                  <button
-                    type="submit"
-                    className="bish-bg-blue py-2 rounded-3xl w-full bish-text-white shadow-lg "
-                  >
-                    S'inscrire
-                  </button>
-                </div>
-              </div>
-            </Form>
+              </Form>
+            )}
           </Formik>
         </div>
         <div className=" flex justify-center items-center bish-bg-blue-opacity border-t rounded-b-3xl border-black w-full">
-          <img className="hidden sm:block pr-10" src={loginSVG} alt="Connexion" />
+          <img
+            className="hidden sm:block pr-10"
+            src={loginSVG}
+            alt="Connexion"
+          />
           <div className="flex flex-col my-6">
-          <span>Déja un compte ?</span>
-          <Link to={URL_LOGIN}
-            className="bish-bg-blue py-2 rounded-3xl bish-text-white shadow-lg mt-2 text-center"
-          >
-           Connexion
-          </Link>
+            <span>Déja un compte ?</span>
+            <Link
+              to={URL_LOGIN}
+              className="bish-bg-blue py-2 rounded-3xl bish-text-white shadow-lg mt-2 text-center"
+            >
+              Connexion
+            </Link>
           </div>
         </div>
       </div>
