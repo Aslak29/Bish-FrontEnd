7e87136e3629b0fc52ff9e580ff7acd9b8d133a2
FlorentN <floneuville@gmail.com>
2022-11-17 13:39:05 +0100

    urlBackEnd.js

--- src/app/constants/urls/urlBackEnd.js
+++ src/app/constants/urls/urlBackEnd.js
@@ -1,2 +1,4 @@
-export const URL_BACK_AUTHENTICATE = '/login_check';
-export const URL_BACK_BLOG = '/blog';
+export const URL_BACK_AUTHENTICATE = "/login_check";
+export const URL_BACK_BLOG = "/blog";
+
+export const URL_BACK_REGISTER = 'user/register'
