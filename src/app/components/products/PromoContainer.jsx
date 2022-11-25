a3dee0d0d3f38f9270e0637d5aad79d700706f53
Aslak29 <alexandre.cholewa@outlook.fr>
2022-11-25 10:54:32 +0100

    PromoCart

---
+++ src/app/components/products/PromoContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PromoCard from '../../components/products/PromoCard';
+
+const PromoContainer = () => {
+  return (
+    <div>PromoContainer</div>
+  )
+}
+
+export default PromoContainer
