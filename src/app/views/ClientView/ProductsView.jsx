aadb1c4fd76895334a02e5d3dcef570baaeffaae
Romain H <romain.hermant62@gmail.com>
2022-11-25 17:46:48 +0100

    Composant produit + produits section

--- src/app/views/ClientView/ProductsView.jsx
+++ src/app/views/ClientView/ProductsView.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Helmet} from "react-helmet-async";
+import ProductsContainer from './../../components/products/ProductsContainer';
 
 const ProductsView = () => {
-    return <div className="">
-        <Helmet>
-            <title>Bish - Nos Produits</title>
-            <meta name="description" content="Retrouvez tous nos produits disponibles à tout moments"/>
-        </Helmet>
-        Products View</div>;
+    return (
+        <div className="w-11/12 lg:w-3/4 mx-auto my-6">
+            <Helmet>
+                <title>Bish - Nos Produits</title>
+                <meta name="description" content="Retrouvez tous nos produits disponibles à tout moments"/>
+            </Helmet>
+            <ProductsContainer/>
+        </div>
+    );
 };
 
 export default ProductsView;
