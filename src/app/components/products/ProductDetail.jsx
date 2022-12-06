33874c0c1516a2dfcd854794839c8385213a4c3c
FlorentN <floneuville@gmail.com>
2022-12-06 09:03:29 +0100

    starsComponent fini

--- src/app/components/products/ProductDetail.jsx
+++ src/app/components/products/ProductDetail.jsx
@@ -1,58 +1,70 @@
-import React from 'react'
+import React from "react";
 import caddie from "../../assets/images/caddie.png";
-import {Helmet} from "react-helmet-async";
-import { useEffect } from 'react';
-import Taille from './Taille';
-const ProductDetail = props => {
-    
-    return (
-            <div className="flex flex-col lg:flex-row justify-center bish-bg-product-detail rounded-lg">
-                <Helmet>
-                    <title>Bish - {props.name}</title>
-                    <meta name="description" content={props.description}/>
-                </Helmet>
+import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
+import Taille from "./Taille";
+import StarsComponent from "./StarsComponent";
 
+const ProductDetail = (props) => {
+  return (
+    <div className="flex flex-col lg:flex-row justify-center bish-bg-product-detail rounded-lg">
+      <Helmet>
+        <title>Bish - {props.name}</title>
+        <meta name="description" content={props.description} />
+      </Helmet>
 
-                <div className="p-7 w-3/4 md:w-3/5 lg:w-1/3 h-96 md:h-full w-fit m-auto">
-                    <img className="w-full h-full object-cover border-solid border-2 bish-border-gray" src={window.location.origin + '/src/app/assets/images/products/' + props.pathImage} alt={props.name}/>
-                    {/*TODO: ajouter la source du produits*/}
-                </div>
+      <div className="p-7 w-3/4 md:w-3/5 lg:w-1/3 h-96 md:h-full w-fit m-auto">
+        <img
+          className="w-full h-full object-cover border-solid border-2 bish-border-gray"
+          src={
+            window.location.origin +
+            "/src/app/assets/images/products/" +
+            props.pathImage
+          }
+          alt={props.name}
+        />
+        {/*TODO: ajouter la source du produits*/}
+      </div>
 
-
-                <div className="p-8 md:w-5/6 w-full w-11/12 m-auto lg:m-0 relative md:pt-10 -z-0">
-                    <div className="flex">
-                        <h1 className="text-2xl  md:text-3xl lg:text-4xl">{props.name}</h1>
-                        <div className="flex items-center ml-5">
-                            Note
-                            {/*TODO: ajouter le component Note ici*/}
-                        </div>
-                    </div>
-                    <div className="pt-5 pb-3 bish-text-blue text-sm md:text-2xl">
-
-                        <div>
-                            {(props.promotion.length !== 0 ?
-                                <div>
-                                    <span className='line-through'>{props.price}€</span>
-                                    <span className='font-semibold text-3xl'> {props.promotion.price_remise}€</span>
-                                </div>
-                                :
-                                <span>{props.price} €</span>
-                        )}</div>
-                    </div>
-                    <div className="text-left text-sm md:text-lg">
-                        {props.description}
-                    </div>
-                    <div className="mb-40 mt-8">
-                        <p className="text-sm md:text-lg bish-text-blue">Tailles :</p>
-                        <div className='w-1/2 mt-4 flex flex-row space-x-2 justify-center'>
-                            {Object.entries(props.stockBySize).map(([index, res]) => <Taille key={index} taille={res} addStock={false}/>)}
-                        </div>
-                    </div>
-                    <div className="flex justify-end absolute bottom-10 right-10">
-                        <button className="flex items-center bish-bg-blue rounded-xl bish-text-white shadow-lg p-3">Ajouter <img className="h-4 ml-3" src={caddie} alt=""/></button></div>
-                    </div>
-            </div>
-    )
-}
+      <div className="p-8 md:w-5/6 w-full w-11/12 m-auto lg:m-0 relative md:pt-10 -z-0">
+        <div className="flex">
+          <h1 className="text-2xl  md:text-3xl lg:text-4xl">{props.name}</h1>
+          <div className="flex items-center ml-5">
+            <StarsComponent note={props.noteAverage} />
+          </div>
+        </div>
+        <div className="pt-5 pb-3 bish-text-blue text-sm md:text-2xl">
+          <div>
+            {props.promotion.length !== 0 ? (
+              <div>
+                <span className="line-through">{props.price}€</span>
+                <span className="font-semibold text-3xl">
+                  {" "}
+                  {props.promotion.price_remise}€
+                </span>
+              </div>
+            ) : (
+              <span>{props.price} €</span>
+            )}
+          </div>
+        </div>
+        <div className="text-left text-sm md:text-lg">{props.description}</div>
+        <div className="mb-40 mt-8">
+          <p className="text-sm md:text-lg bish-text-blue">Tailles :</p>
+          <div className="w-1/2 mt-4 flex flex-row space-x-2 justify-center">
+            {Object.entries(props.stockBySize).map(([index, res]) => (
+              <Taille key={index} taille={res} addStock={false} />
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-end absolute bottom-10 right-10">
+          <button className="flex items-center bish-bg-blue rounded-xl bish-text-white shadow-lg p-3">
+            Ajouter <img className="h-4 ml-3" src={caddie} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetail;
--- src/app/views/ClientView/ProductView.jsx
+++ src/app/views/ClientView/ProductView.jsx
@@ -5,7 +5,7 @@ import {URL_BACK_PRODUCT} from "../../constants/urls/urlBackEnd";
 import {useNavigate, useParams} from "react-router-dom";
 import {URL_404} from "../../constants/urls/urlFrontEnd";
 import SuggestionsContainer from "../../components/products/SuggestionsContainer"
-import StarsComponent from "../../components/layouts/StarsComponent"
+
 
 const ProductView = () => {
 
@@ -39,7 +39,7 @@ const ProductView = () => {
     <div className="w-full">
       <div className="w-3/4 m-auto mt-12 mb-12 space-y-12">
       {product && <ProductDetail {...product} />}
-      {<StarsComponent/>}
+  
       {product && (
         <SuggestionsContainer
         id={product.id}
