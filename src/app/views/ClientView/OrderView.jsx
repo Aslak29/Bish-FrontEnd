16a9671442ee5d1987b8e95f74f224134947ec1f
FlorentN <floneuville@gmail.com>
2022-12-09 09:53:18 +0100

    merge sur ma branche

--- src/app/views/ClientView/OrderView.jsx
+++ src/app/views/ClientView/OrderView.jsx
@@ -3,65 +3,37 @@ import {Helmet} from "react-helmet-async";
 
 const OrderView = () => {
   return (
-    <div className="titleAndTableContainer w-full text-center">
-      <Helmet>
-        <title>Bish - Commandes</title>
-      </Helmet>
-      <h1 className="m-5 bish-text-blue">Vos commandes en cours</h1>
-      {/* table Container */}
+    <div>
+      <h1 className="m-5 bish-text-blue text-center">Vos commandes en cours</h1>
       <div className="border-t-3 w-3/4 m-auto">
-        <p className="border bish-text-white bish-bg-blue">
+        <p className="bish-text-blue text-center">
           Numéro de commande : {"555999"} du {"00/00/99"}
         </p>
-        <table className="table-auto bish-shadow-grey w-full m-auto">
-          <thead>
-            <tr>
-              <th className="border bish-text-white bish-bg-gray">
-                {"img"} Description
-              </th>
-              <th className="border bish-text-white bish-bg-gray">Taille</th>
-              <th className="border bish-text-white bish-bg-gray">Quantité</th>
-              <th className="border bish-text-white bish-bg-gray">
-                Prix Unitaire
-              </th>
-              <th className="border bish-text-white bish-bg-gray">Remise</th>
-              <th className="border bish-text-white bish-bg-gray">Total</th>
-            </tr>
-          </thead>
-          <tbody>foreach sur la table commande par id_utilisateur</tbody>
-          {/* container Frais de port, taxes, TOTAL & adresse de livraison*/}
-          <tfoot>
-            <tr>
-              {/* container Frais de port, taxes, TOTAL*/}
-              <th className="bish-text-gray">
-              Sous-total produits
-              {/*manque une balise pour ranger les 3 aspects financiers*/}
-              </th>
-              <td>f</td>
-            </tr>
-            <tr>
-              <th className="bish-text-gray">
-              Frais de port et taxes
-              </th>
-              <td>f</td>
-            </tr>
-            <tr>
-              <th className="bish-text-white bish-bg-blue rounded-xl">
-              TOTAL:
-              </th>
-              <td>45</td>
-            </tr>
-            <tr className="border">
-              <th className="bish-text-gray">Adresse de livraison</th>
-              <tr className="text-left">user_id.rue</tr>
-              <tr className="text-left">user_id.postal_code</tr>
-              <tr className="text-left">user_id.city</tr>
-            </tr>
-          </tfoot>
-        </table>
+    </div>
+    <div className="containerGeneral grid grid-cols-6 gap-6 bish-bg-product-detail w-full bish-shadow-grey m-5">
+      <div className="description bish-text-gray font-medium m-5 text-center">
+        <h6>Description</h6>
+      </div>
+      <div className="taille bish-text-gray font-medium m-5 text-center">
+        <h6>Taille</h6>
+      </div>
+      <div className="quantite bish-text-gray font-medium m-5 text-center">
+        <h6>Quantité</h6>
+      </div>
+      <div className="prix bish-text-gray font-medium m-5 text-center">
+        <h6>Prix</h6>
+      </div>
+      <div className="remise bish-text-gray font-medium m-5 text-center">
+        <h6>Remise</h6>
+      </div>
+      <div className="total bish-text-gray font-medium m-5 text-center">
+        <h6>Total</h6>
       </div>
     </div>
+    </div>
+
+
   );
-}
+  }
 
 export default OrderView
