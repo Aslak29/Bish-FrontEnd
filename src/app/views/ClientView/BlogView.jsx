c007911e84c9168be9bd7095a3c5606cd0415a64
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2022-12-12 16:59:38 +0100

    BlogView et BlogArticleView

--- src/app/views/ClientView/BlogView.jsx
+++ src/app/views/ClientView/BlogView.jsx
@@ -2,33 +2,40 @@ import React, {useEffect, useState} from 'react'
 import apiBackend from "../../api/backend/api.Backend";
 import {URL_BACK_BLOG} from "../../constants/urls/urlBackEnd";
 import {Helmet} from "react-helmet-async";
-
+import { Link } from 'react-router-dom';
 const BlogView = () => {
 
   const [blog,setBlog] = useState([]);
 
   useEffect(() => {
     apiBackend.get(URL_BACK_BLOG).then((response => {
-      setBlog(response.data)
+      setBlog(response.data);
     }))
   },[])
+
   return (
-    <div className='flex flex-col justify-center items-center mt-4 gap-y-8  w-full'>
+    <div className='flex flex-col justify-center items-center mt-4 gap-y-8  w-full mb-12'>
         <Helmet>
             <title>Bish - Blog</title>
             <meta name="description" content="Découvrez les nouveaux article de notre blog !" />
         </Helmet>
-        Nouveaux articles
-        {Object.entries(blog).map((article)=>{
-          const{ title, description, pathImage, date} =article[1];
+        <h6>Nouveaux articles</h6>
+        {Object.entries(blog).slice(0).reverse().map((article)=>{
+          const{id, title, description, pathImage, date} =article[1];
+
           return(
-            <div key={article.id} className="border bish-border-gray  bish-bg-white-up w-11/12 flex flex-row h-72">
-              <img className='w-2/3 object-cover' src={window.location.origin + `/src/app/assets/images/blog` + `${pathImage}`} alt="Illustration d'un article de blog" />
-              <div className='flex flex-col m-6'>
-                <p>{title}</p>
-                <p>{date.date}</p>
-                <p className='text-justify'>{description.substring(0, 80) + "..."}</p>
-                <button className='btn-primary-bish w-auto h-auto'>En savoir plus</button>
+            <div key={id} className="border bish-border-gray bish-bg-white-up w-11/12 flex flex-col sm:flex-row h-auto sm:odd:flex-row-reverse ">
+            {/* <div key={id} className="border bish-border-gray bish-bg-white-up w-11/12 flex flex-col sm:flex-row h-auto sm:odd:flex-row-reverse "> */}
+              <img className='w-full sm:w-3/5 object-cover' src={window.location.origin + `/src/app/assets/images/blog` + `${pathImage}`} alt="Illustration d'un article de blog" />
+              <div className='flex flex-col m-6 justify-around'>
+                <div>
+                  <p>{title}</p>
+                  <p className='bish-text-gray text-sm	'>{date.date}</p><br/>
+                </div>
+                <p className='text-justify text-sm md:text-lg'>{description.substring(0, 80) + "..."}</p><br/>
+                <button >
+                  <Link className='btn-black-bish w-auto h-auto' to={"/blog/article/" +  `${article[1].id}`}>En savoir plus</Link>
+                </button>
               </div>
             </div>
           )
