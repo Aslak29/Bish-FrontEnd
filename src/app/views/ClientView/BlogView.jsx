a7bd1beef913e4420f9b4ecc86fb6881d1757d09
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2022-12-12 09:19:35 +0100

    BlogView

--- src/app/views/ClientView/BlogView.jsx
+++ src/app/views/ClientView/BlogView.jsx
@@ -12,19 +12,27 @@ const BlogView = () => {
       setBlog(response.data)
     }))
   },[])
-
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center mt-4 gap-y-8  w-full'>
         <Helmet>
             <title>Bish - Blog</title>
             <meta name="description" content="Découvrez les nouveaux article de notre blog !" />
         </Helmet>
-      BlogView
-      {blog.map((blogs) => {
-        return (
-            <h1>{blogs.title}</h1>
-        )
-      } )}
+        Nouveaux articles
+        {Object.entries(blog).map((article)=>{
+          const{ title, description, pathImage, date} =article[1];
+          return(
+            <div key={article.id} className="border bish-border-gray  bish-bg-white-up w-11/12 flex flex-row h-72">
+              <img className='w-2/3 object-cover' src={window.location.origin + `/src/app/assets/images/blog` + `${pathImage}`} alt="Illustration d'un article de blog" />
+              <div className='flex flex-col m-6'>
+                <p>{title}</p>
+                <p>{date.date}</p>
+                <p className='text-justify'>{description.substring(0, 80) + "..."}</p>
+                <button className='btn-primary-bish w-auto h-auto'>En savoir plus</button>
+              </div>
+            </div>
+          )
+        })}
     </div>
   )
 }
