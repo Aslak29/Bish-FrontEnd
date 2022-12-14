1b10e0f8372d3d7f779bad1d02f4e46c69816a42
Romain H <romain.hermant62@gmail.com>
2022-12-14 15:41:53 +0100

    Utils inital value product

--- src/app/utils/AdminInitialValues.js
+++ src/app/utils/AdminInitialValues.js
@@ -1,10 +1,42 @@
+// PRODUITS
+// Valeurs initiales du formulaire create admin produit
+export const productCreateInitialValues = stock => {
+    return {
+        name: '',
+        price: '',
+        description: '',
+        stock,
+        categorie: 1,
+        promotion: '-',
+        trend: false,
+        available: false,
+    }
+}
 
-// Valeurs initials du formulaire de création de admin catégorie
+// Valeurs initiales du formulaire create admin produit
+export const productUpdateInitialValues = (produit, stock) => {
+    return {
+        name: produit.name,
+        price: produit.price,
+        description: produit.description,
+        stock,
+        categorie: produit.id_categorie,
+        promotion: produit.promotion.id ? produit.promotion.id : '-',
+        trend: produit.is_trend,
+        available: produit.is_available,
+        infoFile: {
+            name: produit.pathImage
+        }
+    }
+}
+
+// CATEGORIES
+// Valeurs initiales du formulaire create admin categorie
 export const categoryCreateInitialValues = {
         name: '',
         trend: false,
 }
-// Valeurs initials du formulaire de update de admin catégorie
+// Valeurs initiales du formulaire update admin categorie
 export const categoryUpdateInitialValues = (categories) => {
     return {
         name: categories.name,
