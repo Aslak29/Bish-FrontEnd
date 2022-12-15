88f3bac5a88a6fe62bc63a77797db323c697e8d7
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2022-12-15 09:51:07 +0100

    15/12

---
+++ src/app/components/admin/blog/FormUpdate.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import apiBackEnd from "../../../api/backend/api.Backend";
+import {URL_BACK_CATEGORIES_UPDATE} from "../../../constants/urls/urlBackEnd";
+import {toast} from "react-toastify";
+import {categoryUpdateSchema} from "../../../utils/AdminValidationSchema";
+import {categoryUpdateInitialValues} from "../../../utils/AdminInitialValues";
+
+const FormUpdate = props => {
+    // UPDATE élément dans la BDD
+    const pathImageDefault = props.categories.pathImage;
+
+    const updateRow = (id, values) => {
+        console.log(id);
+        if (window.confirm("Êtes-vous sûr de vouloir modifier le produit ?")) {
+            apiBackEnd.post(
+                `${URL_BACK_CATEGORIES_UPDATE}/${id}/${values.name}/${values.trend}/${
+                    values.infoFile !== undefined ? values.infoFile.name : pathImageDefault
+                }`
+            ).then(res => {
+                if (res.status === 200) {
+                    // Notification succès d'une modification de produit
+                    toast.success(`Le produit ${res.data.id} - ${res.data.name} a été modifié!`, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light"
+                    })
+                }
+            }).catch(error => {
+                    console.log(error);
+                    // Notification erreur
+                    toast.error('Une erreur est survenue', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light"
+                    });
+                }
+            )
+        }
+    }
+
+    // Preview de l'image dans input type file
+    const showPreview = e => {
+        if (e.target.files.length > 0) {
+            let src = URL.createObjectURL(e.target.files[0]);
+            let preview = document.getElementById("img-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+
+    return (
+        <Formik
+            initialValues={categoryUpdateInitialValues(props.categories)}
+            validationSchema={categoryUpdateSchema}
+            onSubmit={(values) => updateRow(props.categories.id, values)}
+        >
+            {formikProps =>
+                <Form className="w-[40rem]">
+                    {/* Nom */}
+                    <div className="flex flex-row gap-4 mb-4">
+                        <div className="flex flex-col justify-between items-start">
+                            <div className="flex flex-col h-20 w-full">
+                                <span>Nom</span>
+                                <Field className='h-full w-full' type="text" name="name"/>
+                                <ErrorMessage
+                                    name="name"
+                                    component="small"
+                                    className="text-red-400"
+                                />
+                            </div>
+
+                            <div className="flex flex-row h-20 justify-around">
+                                <div className="flex flex-col h-full justify-center align-items-center">
+                                    <span>Tendance</span>
+                                    <Field className='h-8 w-8 lg:h-10 lg:w-10 bish-text-blue m-auto' type="checkbox"
+                                           name="trend"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col h-20">
+                                <span>Image</span>
+                                <Field className='my-auto' accept="image/*" type="file" name="file" onChange={e => {
+                                    console.log(e.currentTarget.files[0].type)
+                                    showPreview(e);
+                                    formikProps.setFieldValue('infoFile', e.currentTarget.files[0])
+                                }}/>
+                                <ErrorMessage
+                                    name="infoFile"
+                                    component="small"
+                                    className="text-red-400"
+                                />
+                            </div>
+                        </div>
+                        {/* Image */}
+                        {/* Preview de l'image */}
+                        <div className="flex flex-col w-full">
+                            <div className="preview row-span-4 h-96 w-full shadow-lg">
+                                <img className='object-contain h-full w-full' id="img-preview" alt='Prévisualisation'
+                                     src={window.location.origin + '/src/app/assets/images/categories/'
+                                         + props.categories.pathImage}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <button type="submit" className="bish-bg-blue py-3 w-full bish-text-white col-span-4 mx-auto">
+                            Ajouter
+                        </button>
+                    </div>
+                </Form>
+            }
+        </Formik>
+    )
+}
+
+export default FormUpdate
---
+++ src/app/views/AdminView/AdminBlogView.jsx
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import apiBackEnd from '../../api/backend/api.Backend';
+import sortIMG from '../../assets/images/trier.png'
+import addIMG from '../../assets/images/add.png'
+import loadingSVG from '../../assets/images/loading-spin.svg'
+import TableRow from './../../components/admin/TableRow';
+import ModalCrud from '../../components/admin/ModalCrud';
+import { search, sort } from '../../services/adminServices';
+import { Field, Form, Formik } from "formik";
+import { URL_BACK_BLOG } from '../../constants/urls/urlBackEnd';
+import FormUpdate from '../../components/admin/blog/FormUpdate';
+import FormCreate from '../../components/admin/blog/FormCreate';
+
+const AdminBlogView = () => {
+
+    // Style
+    const headSort = 'flex py-5 justify-center items-center space-x-2 cursor-pointer'
+    const labelHeader = 'truncate hover:text-clip'
+
+    // Contenu d'un ligne de la table (sans les keys, que les datas)
+    const [rows, setRows] = useState([])
+    // Formulaire UPDATE
+    const [formUpdate, setFormUpdate] = useState([])
+    // Formulaire CREATE
+    const [formCreate, setFormCreate] = useState()
+    // SVG isLoading si requête en cours
+    const [isLoading, setIsLoading] = useState(false);
+    // State modal CREATE
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    useEffect(() => {
+        // Permet d'afficher le SVG de chargement
+        setIsLoading(true)
+        // Récupération des données
+        axios.all([
+        // TODO: SI PLUSIEURS APPEL API: METTRE ICI
+        apiBackEnd.get(URL_BACK_BLOG)
+        ])
+        .then(respArr => {
+            console.log(respArr);
+        // TODO: EXEMPLE: METTRE LES ELEMENT DANS L'ORDRE D'AFFICHAGE DANS UNE ROW
+        // Set le contenu d'une row (à mettre dans l'ordre voulu)
+        respArr[0].data.map((res) => setRows(current => [...current, [
+            res.id,
+            res.title,
+            res.description,
+            <img className='object-contain h-10 m-auto hover:absolute hover:scale-[10.0] hover:z-50' src={window.location.origin + '/src/app/assets/images/blog/' + res.pathImage} alt={res.name}/>,
+            // <input className='h-8 w-8 lg:h-10 lg:w-10 bish-text-blue' type="checkbox" defaultChecked={res.is_trend}/>,
+        ]]))
+
+        // respArr[2].data.map((res) => {
+        //     // TODO: FORMULAIRE AFFICHE DANS LE MODAL UPDATE
+        //     // Formulaire UPDATE
+        //     setFormUpdate(current => [...current,
+        //         <Formik
+        //             initialValues={{
+        //             // TODO: DEFINIR LES VALEURS INITIALES
+        //             name: res.id,
+        //             description: res.name
+        //             }}
+        //             onSubmit={(values) => updateRow(res.id, values)}
+        //         >
+        //             <Form className="grid grid-cols-2 sm:grid-cols-4 gap-4">        
+        //                 {/* Nom */}
+        //                 <div className="flex flex-col h-20">
+        //                     <span>Nom</span>
+        //                     <Field className='h-full' type="text" name="name"/>
+        //                 </div>
+        //                 {/* Description */}
+        //                 <div className="flex flex-col col-span-2 row-span-2">
+        //                     <span>Description</span>
+        //                     <Field className='h-full' as="textarea" type="text" name="description" required/>
+        //                 </div>
+        //                 {/* Button Modifier */}
+        //                 <button type="submit" className="bish-bg-blue py-3 w-full bish-text-white col-span-4 mx-auto">Modifier</button>
+        //             </Form>
+        //         </Formik>
+        //     ])
+        // })
+      
+        // TODO: FORMULAIRE AFFICHE DANS LE MODAL CREATE
+        // Formulaire CREATE
+        setFormCreate(
+            <Formik
+                initialValues={{
+                // TODO: DEFINIR LES VALEURS INITIALES
+                name: '',
+                description: '',
+                }}
+                onSubmit={(values) => console.log(values)}
+            >
+                <Form className="grid grid-cols-2 sm:grid-cols-4 gap-4">        
+                        {/* Nom */}
+                        <div className="flex flex-col h-20">
+                            <span>Titre</span>
+                            <Field className='h-full' type="text" name="name"/>
+                        </div>
+                        {/* Description */}
+                        <div className="flex flex-col col-span-2 row-span-2">
+                            <span>Rédigez l'article ici</span>
+                            <Field className='h-full' as="textarea" type="text" name="description" required/>
+                        </div>
+                        {/* Button Modifier */}
+                        <button type="submit" className="bish-bg-blue py-3 w-full bish-text-white col-span-4 mx-auto">Ajouter</button>
+                </Form>
+            </Formik>
+        )
+
+      // Fin du chargement
+      setIsLoading(false)
+    })
+    },[])
+
+    // UPDATE élément dans la BDD
+    const updateRow = (id, values) => {
+      console.log(values);
+    }
+
+    // DELETE élément dans la BDD
+    const deleteRow = id => {
+      console.log(id)
+    }
+
+    // Open modal CREATE
+    function openModal() {
+      setIsOpen(true);
+    }
+
+    // Close modal CREATE
+    function closeModal() {
+      setIsOpen(false);
+    }
+
+  return (
+    <div>
+        <div className='w-full ml-12 sm:ml-64'>
+            {/* TITRE + BUTTON AJOUTER */}
+            <div className='flex flex-row shadow fixed top-0 right-0 mt-20 bish-bg-white w-full z-10'>
+                <div className='w-12 sm:w-72'></div>
+                <div className='flex flex-row justify-between space-x-5 h-20 w-full px-10'>
+                    {/* TODO: NOM DE L'ENTITE */}
+                    <span className='text-center my-auto text-2xl font-medium'>PRODUITS</span>
+                    <input className='w-1/3 h-10 my-auto' type="text" id="searchInput" onKeyUp={() => search()} placeholder="Rechercher.."/>
+                    <button className='my-auto bg-green-600 p-2 bish-text-white font-medium' onClick={() => openModal()}>
+                      <img className='h-5 lg:h-8' src={addIMG} alt="Ajouter" />
+                    </button>
+                </div>
+            </div>
+          {/* Modal CREATE */}
+          <ModalCrud modalIsOpen={modalIsOpen} openModal={openModal} closeModal={closeModal} form={formCreate}/>
+          {/* TABLE PRODUITS */}
+          {isLoading ? (<img className='absolute top-1/3 left-1/2' src={loadingSVG} alt="Chargement"></img>)
+            : 
+            (
+              <table className="table-fixed w-full pl-5 mt-20" id="searchTable">
+                {/* Nom de chaque colonne */}
+                <thead className='border-b-4 bish-border-gray sticky top-40 bish-bg-white shadow'>
+                  <tr>
+                    {/* Tous les titres dans le header de la table */}
+                    {/* TODO: EXEMPLE HEADER SI IL PEUT ETRE TRIER */}
+                    <th onClick={() => sort(0)}>
+                      <div className={headSort}>
+                        <span className={labelHeader} title='Id'>Id</span>
+                        <img className='h-4' src={sortIMG} alt="Trier par ID" />
+                      </div>
+                    </th>
+                    <th className={labelHeader} title='Tendance'>Titre</th>
+                    <th onClick={() => sort(3)}>
+                      <div className={headSort}>
+                        <span className={labelHeader} title='Description'>Description</span>
+                        <img className='h-4' src={sortIMG} alt="Trier par ID" />
+                      </div>
+                    </th>
+                    {/* TODO: EXEMPLE HEADER SI IL PEUT PAS ETRE TRIER */}
+                    <th className={labelHeader} title='Tendance'>Image</th>
+                    {/* <th className={labelHeader} title='Tendance'>Tendance</th> */}
+                    {/* TH Actions à ne pas supprimer */}
+                    <th className={labelHeader} colSpan='2' title='Actions'>Actions</th>
+                  </tr>
+                </thead>
+                {/* Contenu de la table */}
+                <tbody>
+                  {/* Retourne une ligne pour chaque élément */}
+                  {rows && rows.map((res, index) => <TableRow key={index} element={res} formUpdate={formUpdate[index]} deleteRow={deleteRow}/>)}
+                </tbody>
+              </table>
+            )
+          }
+        </div>
+    </div>
+  )
+}
+
+export default AdminBlogView
