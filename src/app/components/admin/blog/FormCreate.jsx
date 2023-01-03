3b63539e90a7df6bd7e8350bde46573557a506a0
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2023-01-03 09:19:49 +0100

    Màj 03/01

--- src/app/components/admin/blog/FormCreate.jsx
+++ src/app/components/admin/blog/FormCreate.jsx
@@ -8,6 +8,7 @@ import { blogCreateInitialValues } from "../../../utils/AdminInitialValues";
 import ReactQuill,{ Quill } from 'react-quill';
 import * as Emoji from "quill-emoji";
 import 'react-quill/dist/quill.snow.css';
+import "quill-emoji/dist/quill-emoji.css";
 
 Quill.register("modules/emoji", Emoji);
 
@@ -55,13 +56,35 @@ const FormCreate = props => {
             )
         }
     }
-    const toolbarOptions = {
+    // const toolbarOptions = {
+    //     container: [
+    //       ['bold', 'italic', 'underline', 'strike'],
+    //       ['emoji'],   
+    //     ],
+    //     handlers: {'emoji': function() {}}
+    //   }
+    const toolbarOptions= {
         container: [
-          ['bold', 'italic', 'underline', 'strike'],
-          ['emoji'],   
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'font': [] }],
+            ["bold", "italic", "underline", "strike", "blockquote", "link"],
+            [ { 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'] }, { 'background': [] }], 
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            [{ 'align': [] }],
+            ["emoji"],
+            ["clean"]
         ],
-        handlers: {'emoji': function() {}}
+        handlers: {
+            'emoji': function() {},
+            'color': function (value) {
+                if (value == 'custom-color') value = window.prompt('Enter Hex Color Code');
+                this.quill.format('color', value)
+        }
       }
+    }
+
+
     // Preview de l'image dans input type file
     const showPreview = e => {
         if (e.target.files.length > 0) {
@@ -80,50 +103,48 @@ const FormCreate = props => {
             }}
         >
             {formikProps =>
-                <Form className="w-[40rem]">
-                    {/* TITRE */}
-                    <div className="flex flex-row gap-4 mb-4">
-                        <div className="flex flex-col justify-between items-start">
+                <Form className="w-[60rem]">
+                    <div className="flex flex-col">
 
-                            <div className="flex flex-col h-20 w-full">
-                                <span>Titre</span>
-                                <Field className='h-full w-full' type="text" name="title"/>
-                                <ErrorMessage
-                                    name="title"
-                                    component="small"
-                                    className="text-red-400"
-                                />
-                            </div>
+                        {/* TITRE */}
+                        <div className="flex flex-col">
+                            <span>Titre</span>
+                            <Field className='w-full' type="text" name="title"/>
+                            <ErrorMessage
+                                name="title"
+                                component="small"
+                                className="text-red-400"
+                            />
+                        </div>
 
-                            {/* Description */}
-                            <div className="flex flex-col col-span-2 row-span-2 w-96 h-96">
-                                <span>Description</span>
-                                <Field name="description">
-                                    {({field})=> (<ReactQuill 
-                                    value={field.value} 
-                                    className="w-96 h-96" 
-                                    onChange={field.onChange(field.name)} 
-                                    defaultValue=""
-                                    theme="snow"
-                                    modules={{
-                                      toolbar: toolbarOptions,
-                                      "emoji-toolbar": true,
-                                      "emoji-textarea": true,
-                                      "emoji-shortname": true,
-                                    }}
-                                    // value={quill_data.delta}
-                                    
-                                    
-                                    
-                                    
-                                    />)}
-                                </Field>
-                                <ErrorMessage name="description" component="small" className="text-red-400"/>
-                            </div>
-                        
-                            {/* Image */}
-                            <div className="flex flex-col h-20">
+                        {/* Description */}
+                        <div className="flex flex-col h-96">
+                            <span>Description</span>
+                            <Field name="description">
+                                {({field})=> (<ReactQuill 
+                                className="w-full h-80" 
+                                value={field.value} 
+                                onChange={field.onChange(field.name)} 
+                                defaultValue=""
+                                theme="snow"
+                                modules={{
+                                    toolbar: toolbarOptions,
+                                    "emoji-toolbar": true,
+                                    "emoji-textarea": false,
+                                    "emoji-shortname": true,
+                                }}                                    
+                                
+                                
+                                />)}
+                            </Field>
+                            <ErrorMessage name="description" component="small" className="text-red-400"/>
+                        </div>
+                    
+                        {/* Image */}
+                        <div className="flex flex-col w-full">
+                            <div className="flex flex-col">
                                 <span>Image</span>
+                                {/* Formulaire ajout image */}
                                 <Field className='my-auto' accept="image/*" type="file" name="file" onChange={e => {
                                     showPreview(e);
                                     formikProps.setFieldValue('infoFile', e.currentTarget.files[0])
@@ -134,17 +155,15 @@ const FormCreate = props => {
                                     className="text-red-400"
                                 />
                             </div>
-                        </div>
-                        {/* Image */}
-                        {/* Preview de l'image */}
-                        <div className="flex flex-col w-full">
-                            <div className="preview row-span-4 h-96 w-full shadow-lg">
-                                <img className='hidden object-contain h-full w-full' id="img-preview"
-                                        alt='Prévisualisation'
-                                />
+                            {/* Preview de l'image */}
+                            <div className="flex flex-col w-full">
+                                <div className="preview row-span-4 h-96 w-full shadow-lg">
+                                    <img className='hidden object-contain h-full w-full' id="img-preview"alt='Prévisualisation'/>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                     <div className="w-full">
                         <button type="submit" className="bish-bg-blue py-3 w-full bish-text-white col-span-4 mx-auto">
                             Ajouter
