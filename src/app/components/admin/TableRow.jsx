806f6343b73f23a7b941c08dda42794217711e91
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2023-01-10 15:00:56 +0100

    Correction hover description blog

--- src/app/components/admin/TableRow.jsx
+++ src/app/components/admin/TableRow.jsx
@@ -18,7 +18,7 @@ const TableRow = props => {
         setIsOpen(false);
     }
     return (<tr className='border-b bish-border-gray'>
-        {props.element && props.element.map((res, index) => <td key={index} className='truncate hover:text-clip	text-center py-5' title={res}>{res}</td>)}
+        {props.element && props.element.map((res, index) => <td key={index} className='truncate hover:text-clip	text-center py-5' title={typeof res === 'object'? '': res}>{res}</td>)}
         <td colSpan='2' className='text-center space-x-5'>
             {!props.disabledEdit ?
                 <button className='bg-orange-500 p-2 bish-text-white font-medium' onClick={() => openModal()}>
