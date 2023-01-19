2f3ed9b96c85f0b7139398cc2ecb1fbe34afac87
Romain H <romain.hermant62@gmail.com>
2023-01-19 10:20:46 +0100

    Link sur chaque etape

--- src/app/views/ClientView/CartValidation/Resume.jsx
+++ src/app/views/ClientView/CartValidation/Resume.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useStep } from './CartOutletValidation';
 
 const Resume = () => {
+
+  const { setStep } = useStep();
+
+  useEffect(() => {
+    setStep(3)
+  },[])
+
   return (
     <div>Resume</div>
   )
