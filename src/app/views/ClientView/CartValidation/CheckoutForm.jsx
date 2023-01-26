e05cf58e748b28c574b5aa096b698d150735432e
Aslak29 <alexandre.cholewa@outlook.fr>
2023-01-26 16:03:21 +0100

    fini

--- src/app/views/ClientView/CartValidation/CheckoutForm.jsx
+++ src/app/views/ClientView/CartValidation/CheckoutForm.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import {PaymentElement, useElements, useStripe} from "@stripe/react-stripe-js";
 import apiBackEnd from "@/app/api/backend/api.Backend";
 import {URL_UPDATE_PAYMENTMETHOD} from "@/app/constants/urls/urlBackEnd";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectIdPaymentIntent} from "@/app/redux-store/cartSlice";
 import {useNavigate} from "react-router-dom";
 import {URL_CART_RESUME} from "@/app/constants/urls/urlFrontEnd";
+import { clearItems } from '../../../redux-store/cartSlice';
+import { useState } from 'react';
 
 const CheckoutForm = () => {
 
     const navigate = useNavigate()
-
+    const dispatch = useDispatch();
     const stripe = useStripe();
     const elements = useElements();
+    const [error, setError] = useState();
 
     const idPaymentId = useSelector(selectIdPaymentIntent)
     const handleSubmit = async (event) => {
@@ -20,20 +23,26 @@ const CheckoutForm = () => {
         if (!stripe || !elements) {
             return;
         }
-        await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement('card')
-        }).then(res => {
-            apiBackEnd.post(URL_UPDATE_PAYMENTMETHOD + idPaymentId.id +'/'+res.paymentMethod.id).then(res => {
-                navigate(URL_CART_RESUME)
-            })
+        const {error,paymentIntent} = await stripe.confirmPayment({
+           elements,
+            confirmParams:{return_url:`${window.location.origin}/panier/validation/resume`},
+            redirect:"if_required"
         });
+        if(error){
+            setError(error.message)
+        }else if(paymentIntent && paymentIntent.status === "succeeded"){
+            dispatch(clearItems())
+            navigate(URL_CART_RESUME)
+        }else{
+            setError("une erreur est survenue")
+        }
     }
 
     return (
         <div >
             <form onSubmit={handleSubmit} className={"my-20"}>
                 <PaymentElement />
+                {error && <span>{error}</span>}
                 <button className={"border bish-border-gray rounded-2xl px-4 py-2 justify-center bish-bg-blue my-4 w-24"}>Continuer</button>
             </form>
         </div>
