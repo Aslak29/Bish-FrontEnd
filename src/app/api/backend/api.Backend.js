5a83ca2afc9bc60e88b2563713074ce256cafca7
Caudron <antoinecaudron8@gmail.com>
2023-01-27 13:47:37 +0100

    Change route in && give token jwt in axios.create

--- src/app/api/backend/api.Backend.js
+++ src/app/api/backend/api.Backend.js
@@ -1,11 +1,18 @@
 import axios from "axios";
+import localStorage from "redux-persist/es/storage";
 
 /**
  * Instance axios to the BACKEND
  *
  * @author Peter Mollet
  */
+
+let token = await localStorage.getItem('token')
+
 const apiBackEnd = axios.create({
   baseURL: import.meta.env.VITE_REQUEST_API,
+  headers:{
+    Authorization : 'Bearer ' + token
+  }
 });
 export default apiBackEnd;
